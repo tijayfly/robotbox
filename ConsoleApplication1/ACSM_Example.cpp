6fc6a307d332ed48525bc9078092f1b2f969c2ba
TJ Mortimer <tj.mortimer@gmail.com>
2021-06-14 00:49:17 +0100

    Add files via upload

---
+++ ConsoleApplication1/ACSM_Example.cpp
@@ -0,0 +1,202 @@
+// BetterShifter for AC - TJ Mortimer 2021
+// Built on assettocorsamods Shared Memory Example https://assettocorsamods.net/threads/doc-shared-memory-reference.58/
+
+#include "stdafx.h"
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include "SharedFileOut.h"
+
+using namespace std;
+
+
+template <typename T, unsigned S>
+inline unsigned arraysize(const T(&v)[S])
+{
+	return S;
+}
+
+
+struct SMElement
+{
+	HANDLE hMapFile;
+	unsigned char* mapFileBuffer;
+};
+
+SMElement m_graphics;
+SMElement m_physics;
+SMElement m_static;
+
+void initPhysics()
+{
+	TCHAR szName[] = TEXT("Local\\acpmf_physics");
+	m_physics.hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(SPageFilePhysics), szName);
+	if (!m_physics.hMapFile)
+	{
+		MessageBoxA(GetActiveWindow(), "CreateFileMapping failed", "ACS", MB_OK);
+	}
+	m_physics.mapFileBuffer = (unsigned char*)MapViewOfFile(m_physics.hMapFile, FILE_MAP_READ, 0, 0, sizeof(SPageFilePhysics));
+	if (!m_physics.mapFileBuffer)
+	{
+		MessageBoxA(GetActiveWindow(), "MapViewOfFile failed", "ACS", MB_OK);
+	}
+}
+
+void initGraphics()
+{
+	TCHAR szName[] = TEXT("Local\\acpmf_graphics");
+	m_graphics.hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(SPageFileGraphic), szName);
+	if (!m_graphics.hMapFile)
+	{
+		MessageBoxA(GetActiveWindow(), "CreateFileMapping failed", "ACS", MB_OK);
+	}
+	m_graphics.mapFileBuffer = (unsigned char*)MapViewOfFile(m_graphics.hMapFile, FILE_MAP_READ, 0, 0, sizeof(SPageFileGraphic));
+	if (!m_graphics.mapFileBuffer)
+	{
+		MessageBoxA(GetActiveWindow(), "MapViewOfFile failed", "ACS", MB_OK);
+	}
+}
+
+void initStatic()
+{
+	TCHAR szName[] = TEXT("Local\\acpmf_static");
+	m_static.hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(SPageFileStatic), szName);
+	if (!m_static.hMapFile)
+	{
+		MessageBoxA(GetActiveWindow(), "CreateFileMapping failed", "ACS", MB_OK);
+	}
+	m_static.mapFileBuffer = (unsigned char*)MapViewOfFile(m_static.hMapFile, FILE_MAP_READ, 0, 0, sizeof(SPageFileStatic));
+	if (!m_static.mapFileBuffer)
+	{
+		MessageBoxA(GetActiveWindow(), "MapViewOfFile failed", "ACS", MB_OK);
+	}
+}
+
+void Clear()
+{
+#if defined _WIN32
+	system("cls");
+#endif
+}
+
+void mainloop()
+{
+
+	Clear();
+
+	SPageFilePhysics* pf = (SPageFilePhysics*)m_physics.mapFileBuffer;
+	SPageFileStatic* pfs = (SPageFileStatic*)m_static.mapFileBuffer;
+	int onepercent = pfs->maxRpm / 100;
+	int upcent = onepercent * 90;
+	int upcentfirst = onepercent * 95;
+	int downcent = onepercent * 50;
+	INPUT ip;
+	// Set up a generic keyboard event.
+	ip.type = INPUT_KEYBOARD;
+	ip.ki.wScan = 0; // hardware scan code for key
+	ip.ki.time = 0;
+	ip.ki.dwExtraInfo = 0;
+
+	// R is gear 0, N is gear 1, 1st is gear2, 2nd is gear3 etc
+
+	if ((pf->rpms > upcent) & (pf->gear > 2)) {
+		cout << ("UP");
+
+		// Press the "up" key
+		ip.ki.wVk = 0x20; 
+		ip.ki.dwFlags = 0; // 0 for key press
+		SendInput(1, &ip, sizeof(INPUT));
+
+		// Release the "up" key
+		ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+		SendInput(1, &ip, sizeof(INPUT));
+
+		Sleep(500);
+		mainloop();
+	}
+	else if ((pf->rpms > upcentfirst) & (pf->gear == 2)) {
+		cout << ("UP");
+
+		// Press the "up" key
+		ip.ki.wVk = 0x20;
+		ip.ki.dwFlags = 0; // 0 for key press
+		SendInput(1, &ip, sizeof(INPUT));
+
+		// Release the "up" key
+		ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+		SendInput(1, &ip, sizeof(INPUT));
+
+		Sleep(1500);
+		mainloop();
+	}
+	else if ((pf->rpms < downcent) & (pf->gear > 2)) {
+		cout << ("DOWN");
+
+		// Press the "down" key
+		ip.ki.wVk = 0x11; 
+		ip.ki.dwFlags = 0; // 0 for key press
+		SendInput(1, &ip, sizeof(INPUT));
+
+		// Release the "down" key
+		ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+		SendInput(1, &ip, sizeof(INPUT));
+
+		Sleep(100);
+		mainloop();
+	}
+	else {
+		Sleep(100);
+		mainloop();
+	}
+}
+
+void dismiss(SMElement element)
+{
+	UnmapViewOfFile(element.mapFileBuffer);
+	CloseHandle(element.hMapFile);
+}
+
+void printData(string name, float value)
+{
+	wcout << name.c_str() << " : " << value << endl;
+}
+
+template <typename T, unsigned S>
+inline void printData(const string name, const T(&v)[S])
+{
+	wcout << name.c_str() << " : ";
+	for (int i = 0; i < S; i++)
+	{
+		wcout << v[i];
+		if (i < S - 1)
+		{
+			wcout << " , ";
+		}
+	}
+	wcout << endl;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	initPhysics();
+	initGraphics();
+	initStatic();
+
+	while (true)
+	{
+		if (GetAsyncKeyState(0x31) != 0) // user pressed 1
+		{
+			cout << ("INIT");
+			mainloop();
+		}
+
+	}
+
+	dismiss(m_graphics);
+	dismiss(m_physics);
+	dismiss(m_static);
+
+	return 0;
+}
+
+
